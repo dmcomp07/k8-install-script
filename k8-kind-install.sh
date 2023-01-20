1f29306c1697e7270333851e3c122094b7d4eef4
dmcomp07 <97079719+dmcomp07@users.noreply.github.com>
2023-01-20 12:02:30 +0530

    Update k8-kind-install.sh

--- k8-kind-install.sh
+++ k8-kind-install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-echo "This script Install Kubernetes KIND on CentOS"
+echo "
+
+        #############################################################
+        #                                                           #
+        #       This Script Install Kubernetes KIND on CentOS       #
+        #                                                           #
+        #############################################################
+
+
+"
 
 # Check for hardware prerequisites
 mem_size=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
