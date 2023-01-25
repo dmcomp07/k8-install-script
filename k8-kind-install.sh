6f1c05e01a45878448200fe5a46331fda0078845
Nav Sagar <97079719+dmcomp07@users.noreply.github.com>
2023-01-25 19:39:06 +0530

    Update k8-kind-install.sh

--- k8-kind-install.sh
+++ k8-kind-install.sh
@@ -219,6 +219,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl software-proper
 # Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
+apt-get -y install docker.io
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
