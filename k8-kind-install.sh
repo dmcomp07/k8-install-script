9a605b3339b757e2e8366d5fc1effe48c0c2a6d6
Nav Sagar <97079719+dmcomp07@users.noreply.github.com>
2023-01-25 12:13:15 +0530

    Update k8-kind-install.sh

--- k8-kind-install.sh
+++ k8-kind-install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# check the OS
+if [ -f /etc/redhat-release ]; then
+    # code for CentOS
+    echo "Running on CentOS"
+cat << EOF > k8.sh
+#!/bin/bash
+
+
 echo "
 
         #############################################################
@@ -12,38 +20,38 @@ echo "
 "
 
 # Check for hardware prerequisites
-mem_size=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
+mem_size=\$(cat /proc/meminfo | grep MemTotal | awk '{print \$2}')
 echo "Minimum memory required : 2097152 KB"
-echo "Available memory : $mem_size KB "
-if [[ $mem_size -lt 2097152 ]]; then
+echo "Available memory : \$mem_size KB "
+if [[ \$mem_size -lt 2097152 ]]; then
   echo "Error: Your system does not meet the minimum memory requirement of 2GB " >&2
   exit 1
 fi
 
-num_cpus=$(nproc)
+num_cpus=\$(nproc)
 echo "Minimum CPU cores required : 2 cores"
-echo "Available CPU cores : $num_cpus cores"
-if [[ $num_cpus -lt 2 ]]; then
+echo "Available CPU cores : \$num_cpus cores"
+if [[ \$num_cpus -lt 2 ]]; then
   echo "Error: Your system does not meet the minimum CPU requirement of 2 cores " >&2
   exit 1
 fi
 
-
 # Confirm with the user before proceeding
 read -p "Do you want to proceed with the installation ? (y/n) " -n 1 -r
 echo   
-if [[ ! $REPLY =~ ^[Yy]$ ]]
+if [[ ! \$REPLY =~ ^[Yy]\$ ]]
 then
     exit 1
 fi
 
 
+
 # Check for software prerequisites
-if ! [ -x "$(command -v curl)" ]; then
+if ! [ -x "\$(command -v curl)" ]; then
   echo 'Error: curl is not installed.' >&2
   exit 1
 fi
-if ! [ -x "$(command -v yum)" ]; then
+if ! [ -x "\$(command -v yum)" ]; then
   echo 'Error: yum is not installed.' >&2
   exit 1
 fi
@@ -51,8 +59,8 @@ fi
 # Get the hostname
 echo "Enter the hostname:"
 read hostname
-hostnamectl set-hostname $hostname
-echo "`ip route get 1 | awk '{print $NF;exit}'` $hostname" >> /etc/hosts
+hostnamectl set-hostname \$hostname
+echo "`ip route get 1 | awk '{print \$NF;exit}'` \$hostname" >> /etc/hosts
 
 
 # Update system
@@ -83,7 +91,7 @@ systemctl restart containerd
 chkconfig --add containerd
 
 # Add Kubernetes repository
-cat << EOF > /etc/yum.repos.d/kubernetes.repo
+printf "
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -92,9 +100,8 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
         https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
 
-
+" > /etc/yum.repos.d/kubernetes.repo
 
 # Install Kubernetes components
 yum install -y kubectl
@@ -106,32 +113,171 @@ systemctl enable --now kubelet
 curl -Lo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
 chmod +x kind
 sudo mv kind /usr/local/bin/
-echo "export PATH=$PATH:/usr/local/bin" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/bin" >> ~/.bashrc
 source ~/.bashrc
 
 
 # Verify Kind installation
 kind version
 
-cat <<EOF > config.yml
-# 4nodes (two worker & two master) cluster config
+
+
+#three nodes (two worker & one master) cluster config
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 
 nodes:
 - role: control-plane
+- role: control-plane
 - role: worker
 - role: worker
 
-EOF
+
+" > config.yml
+
 
 # Create a cluster
 kind create cluster --config=config.yml
 
 # Verify installation
 kubectl cluster-info --context kind-kind
-
 echo " Kind cluster created with 2 Master + 2 workers "
-
 #nodes list
 kubectl get nodes 
+
+EOF
+
+chmod +x k8.sh
+./k8.sh
+
+
+	
+	
+	
+elif [ -f /etc/lsb-release ]; then
+    # code for Ubuntu
+    echo "Running on Ubuntu"
+cat << EOF > k8.sh
+#!/bin/bash
+
+echo "
+        #############################################################
+        #                                                           #
+        #       This Script Install Kubernetes KIND on Ubuntu       #
+        #                                                           #
+        #############################################################
+"
+
+# Check for hardware prerequisites
+mem_size=\$(cat /proc/meminfo | grep MemTotal | awk '{print \$2}')
+echo "Minimum memory required : 2097152 KB"
+echo "Available memory : \$mem_size KB "
+if [[ \$mem_size -lt 2097152 ]]; then
+  echo "Error: Your system does not meet the minimum memory requirement of 2GB " >&2
+  exit 1
+fi
+
+num_cpus=\$(nproc)
+echo "Minimum CPU cores required : 2 cores"
+echo "Available CPU cores : \$num_cpus cores"
+if [[ \$num_cpus -lt 2 ]]; then
+  echo "Error: Your system does not meet the minimum CPU requirement of 2 cores " >&2
+  exit 1
+fi
+
+# Confirm with the user before proceeding
+read -p "Do you want to proceed with the installation ? y/n " -n 1 -r
+echo   
+if [[ ! \$REPLY =~ ^[Yy]\$ ]]
+then
+    exit 1
+fi
+
+# Check for software prerequisites
+if ! [ -x "\$(command -v curl)" ]; then
+  echo 'Error: curl is not installed.' >&2
+  exit 1
+fi
+if ! [ -x "\$(command -v apt-get)" ]; then
+  echo 'Error: apt-get is not installed.' >&2
+  exit 1
+fi
+
+# Get the hostname
+echo "Enter the hostname:"
+read hostname
+hostnamectl set-hostname \$hostname
+echo "`ip route get 1 | awk '{print \$NF;exit}'` \$hostname" >> /etc/hosts
+
+# Update system
+sudo apt-get update -y
+
+# Install dependencies
+sudo apt-get install -y conntrack socat
+
+# Install necessary packages
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+
+# Install docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+systemctl start docker
+systemctl enable docker
+
+# Add Kubernetes repository
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update -y
+
+# Install Kubernetes components
+sudo apt-get install -y kubectl
+
+# Enable and start kubelet service
+sudo systemctl enable --now kubelet
+
+# Download and install Kind
+curl -Lo kind https://kind.sigs.k8s.io/dl/v0.11.0/kind-linux-amd64
+chmod +x kind
+sudo mv kind /usr/local/bin/
+echo "export PATH=\$PATH:/usr/local/bin" >> ~/.bashrc
+source ~/.bashrc
+
+# Verify Kind installation
+kind version
+
+printf "
+#three nodes (two worker & one master) cluster config
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+
+nodes:
+- role: control-plane
+- role: control-plane
+- role: worker
+- role: worker
+" > config.yml
+
+
+
+# Create a cluster
+kind create cluster --config=config.yml
+
+# Verify installation
+kubectl cluster-info --context kind-kind
+echo " Kind cluster created with 2 Master + 2 workers "
+#nodes list
+kubectl get nodes
+EOF
+
+chmod +x k8.sh
+./k8.sh	
+	
+	
+else
+    # code for other OS
+    echo "Not a supported OS"
+    exit
+fi
